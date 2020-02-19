bb72ccf5e0f4928bfda1f30658e5204f796168c5
Viorel Rinja <viorel.rinja@gmail.com>
2020-02-19 09:38:03 +0200

    initial commit

---
+++ scripts/create-image.sh
@@ -0,0 +1,12 @@
+# https://gist.github.com/toelen/5221051
+
+SRC_SIZE=$(stat -c%s "$1")
+SIZE=$(expr 1474560 - $SRC_SIZE)
+dd bs=$SIZE seek=1 of=nullbytes count=0
+cat $1 nullbytes > $1.flp
+rm ./nullbytes
+
+VBoxManage storagectl test --name floppy --remove
+VBoxManage storagectl test --add floppy --name floppy --controller I82078 --bootable on
+VBoxManage storageattach test --storagectl floppy --port 0 --device 0 --type fdd --medium "$1.flp"
+VBoxManage startvm test
